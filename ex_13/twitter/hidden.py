28f93e0d146ea8cf89c3b8e9f6ba42f4e56abff1
gmokyere <edarko359@gmail.com>
2021-05-17 17:52:47 -0400

    Deleted Twitter Access code for privacy

--- ex_13/twitter/hidden.py
+++ ex_13/twitter/hidden.py
@@ -4,8 +4,8 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "O8SQlGvUp5Bx6JeNsGsENbbdn",
-            "consumer_secret": "6nlyV2rQm0BZ2EZRJMWl63JDaeDzk2Az835xSj8PCgVlL5xdVx",
-            
-            "token_key": "1310801394019041281-qTtqiWBHHplgThBYHM8KwTjxOnXp0b",
-            "token_secret": "czccq3EuulsoUEFJUgJCVmacYSGKRgng2qmEcBuRF96G6"}
+    return {"consumer_key": "",
+            "consumer_secret": "",
+
+            "token_key": "",
+            "token_secret": ""}
